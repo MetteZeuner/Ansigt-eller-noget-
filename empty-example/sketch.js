cd4742f49d019ef1806c001dce2011a8c41a7de3
MetteZeuner <mette.zeuner@outlook.dk>
2017-02-15 15:48:21 +0100

    Add files via upload

---
+++ empty-example/sketch.js
@@ -0,0 +1,15 @@
+function setup() {
+createCanvas(800, 600);
+ 
+}
+
+function draw() {
+background(0, 0, 125);
+fill (225, 204, 0);
+ellipse(400, 300, 100, 100);
+ellipse (380, 280, 20, 20);
+ellipse (410, 280, 20, 20); 
+line (400, 290, 410, 305); 
+
+}
+
